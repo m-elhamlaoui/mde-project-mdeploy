a64aae91ddf5894d18290380051fabafbc6dccca
HM2811 <mekkaoui151@gmail.com>
2025-01-02 12:08:08 +0100

    Initial api backend commit

---
+++ backend/src/main/java/com/example/mde/MdeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdeApplication.class, args);
+	}
+
+}
