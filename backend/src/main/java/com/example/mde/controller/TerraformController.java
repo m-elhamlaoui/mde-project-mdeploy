e762041457f69ea05ac76de639fad1ecf8f6aedb
Meryem EL KARATI <elkaratimeryem@gmail.com>
2025-01-06 00:03:37 +0100

    [FIX] Add Cors Configuration

--- backend/src/main/java/com/example/mde/controller/TerraformController.java
+++ backend/src/main/java/com/example/mde/controller/TerraformController.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.example.mde.service.TerraformService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000/")
 @RequestMapping("/api/terraform")
 public class TerraformController {
 
